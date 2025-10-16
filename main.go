3649d729ad3662b1286752b87346b690e01c7da4
Ikingari <david.solorzano@umich.mx>
2025-10-16 12:00:00 -0600

    nueva rama

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 	// Get port from environment or use default
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8081"
 	}
 
 	log.Printf("Server starting on port %s", port)
